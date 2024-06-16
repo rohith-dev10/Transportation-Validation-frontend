107ea117f8dc8cd3120f382ffbb5c34b83b06777
M Rohith <developer.mr101804@gmail.com>
2024-06-17 01:36:04 +0530

    first

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -33,7 +33,7 @@ const Auth = () => {
         const { fullName, email, password, userType } = formData;
         if (isLogin) {
             try {
-                const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+                const res = await axios.post('https://transportation-validation-platform.onrender.com/api/auth/login', { email, password });
                 localStorage.setItem('token', res.data.token);
                 console.log(res);
                 if (!res.data.isVerified) {
